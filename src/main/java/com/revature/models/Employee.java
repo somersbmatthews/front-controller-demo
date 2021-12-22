faef0bd29f08236b21de04859b493526a3d9e6b6
Somers B Matthews <somersbmatthews@gmail.com>
2021-12-22 15:02:02 -0600

    added uuid for employee class to make it serializable

--- src/main/java/com/revature/models/Employee.java
+++ src/main/java/com/revature/models/Employee.java
@@ -1,5 +1,7 @@
 package com.revature.models;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,7 +17,12 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="employees")
-public class Employee {
+public class Employee implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7217264538771111143L;
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
